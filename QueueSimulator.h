f8d2bae0efa1b0554f7177d0c236e61848f2ec09
Bronwyn Herndon <b.h.bronwyn@gmail.com>
2018-02-09 13:49:40 -0600

    Queue Simulator Testing

--- QueueSimulator.h
+++ QueueSimulator.h
@@ -2,6 +2,7 @@
 // Created by bhbro on 2/4/2018.
 //
 
+#include <iostream>
 #ifndef LINKEDLIST_QUEUE_H
 #define LINKEDLIST_QUEUE_H
 
@@ -14,18 +15,27 @@ private:
         node *next;//next refers to the next node starting with the next one after front etc
         node *prev;//previous would refer to the previously added node
     };
+
+    //Node pointer for the beginning and end of the queue
     node *front, *rear;
+
+    //holds the length of the queue, is changed in offer and poll
+    int size;
+
 
 
 public:
     //initiates front and rear
     Queue();
     //Add an item to the rear of the queue
-    boolean offer(int element);
+    void offer(int element);
     //Return element at front of queue without removing it.
     int peek();
     //take the item from the front of the queue
     int poll();
+
+    //accessor for the size of the queue
+    int getSize();
 };
 
 #endif //LINKEDLIST_QUEUE_H
--- Simulator.cpp
+++ Simulator.cpp
@@ -2,25 +2,32 @@
 // Created by Frazzlectra on 2/4/2018.
 //
 #include "QueueSimulator.h"
-#include "LinkedList.h"
+
+using namespace std;
+
+void printQueue( Queue* q );
 
 int main() {
-    list* l = new list();
+
+    Queue* customerTimeQueue;
+
+    customerTimeQueue->getSize();
     int input;
     char choice;
     cout << "Enter a list of integers in order to add them to the linked list. When you are done entering numbers, enter the letter n"<< endl;
     for (;;) {
         cin >> choice;
-        if (choice != 'n') {
-            input = atoi(&choice);
-            l->add(input);
+        if ( choice != 'n' ) {
+            input = atoi( &choice );
+            printQueue( customerTimeQueue );
         }
         else {
             break;
         }
     }
-    l->print();
+    printQueue( customerTimeQueue );
 
+    /*
     int sum = 0;
     double counter = 0;
     node* current = l->head;
@@ -34,8 +41,17 @@ int main() {
 
     cout << "Enter a value you'd like to delete " << endl;
     cin >> input;
-    l->remove(l->search(input));
-    l->print();
+    customerTimeQueue->remove(l->search(input));
+    customerTimeQueue->print();
     return 0;
+
+     */
 }
 
+void printQueue( Queue* q ){
+
+    for (int i = 0; i < q->getSize(); ++i) {
+        //lol this isn't even sort of close to working
+        cout<<q->peek();
+    }
+}
