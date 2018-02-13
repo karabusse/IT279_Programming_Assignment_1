b79c7926f781fd838acd028bbf6b4be6add16f80
Busse, Kara <kbusse1@ilstu.edu>
2018-02-13 12:21:30 -0600

    Completed Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,22 +1,146 @@
+////
+////  Calculator.cpp
+////  StackPractice
+////
+////  Created by David Mancol on 2/11/18.
+////  Copyright © 2018 David Mancol. All rights reserved.
+////
 //
-//  Calculator.cpp
-//  StackPractice
+//#include <iostream>
+//#include "Calculator.h"
+//using namespace std;
 //
-//  Created by David Mancol on 2/11/18.
-//  Copyright © 2018 David Mancol. All rights reserved.
-//
-
+//// Main function
+//int main()
+//{
+//    Calculator::stack s;
+//    s.createHead(0);
+//    s.push();
+//    s.push();
+//    s.show();
+//    return 0;
+//}
+#include <algorithm>
+#include <set>
+#include <string>
+#include <stack>
 #include <iostream>
-#include "Calculator.h"
+
 using namespace std;
 
-// Main function
-int main()
-{
-    Calculator::stack s;
-    s.createHead(0);
-    s.push();
-    s.push();
-    s.show();
-    return 0;
+template<class T>
+class Node {
+public:
+    T data;
+    Node<T> *next;
+};
+
+template<class T>
+class Stack {
+public:
+    Stack();
+    int size;
+    Node<T> *top;
+    void push(T val);
+    T pop();
+    T peek();
+};
+
+template<class T>
+Stack<T>::Stack() {
+    size = 0;
+    top = NULL;
+}
+
+template<class T>
+void Stack<T>::push(T val) {
+    auto *n = new Node<T>;
+    n->data = val;
+    if (top) {
+        n->next = top;
+    } else {
+        n->next = NULL;
+    }
+    top = n;
+    size++;
+}
+
+template<class T>
+T Stack<T>::pop() {
+    T val = top->data;
+    if (top->next == NULL) {
+        delete top;
+    } else {
+        Node<T> *tmp = top->next;
+        delete top;
+        top = tmp;
+    }
+    size--;
+    return val;
+}
+
+template<class T>
+T Stack<T>::peek() {
+    return top->data;
+}
+
+int main() {
+    auto *activeStack = new Stack<int>;
+    auto *inactiveStack = new Stack<int>;
+    string arr[] = {"+", "-", "*", "/", "%"};
+    std::set<string> operators(arr, arr + sizeof(arr) / sizeof(arr[0]));
+    string input;
+    cout << "> ";
+    cin >> input;
+    std::transform(input.begin(), input.end(), input.begin(), ::tolower);
+    int current = 0;
+    activeStack->push(current);
+    while (input != "q") {
+        Node<int> *root;
+        if (input == "c") {
+            current = 0;
+            activeStack->push(current);
+        } else if (input == "u") {
+            int prev = activeStack->pop();
+            inactiveStack->push(prev);
+            current = activeStack->peek();
+        } else if (input == "r") {
+            if (inactiveStack->size == 0) {
+                cout << "No operations to redo" << endl;
+            } else {
+                current = inactiveStack->pop();
+                activeStack->push(current);
+            }
+        } else {
+            string oper = input.substr(0, 1);
+            if (operators.find(oper) == operators.end()) {
+                cout << "Invalid operator. Please try again." << endl;
+                cin >> input;
+            }
+            int operand = atoi(input.substr(1).c_str());
+            if (!operand) {
+                cout << "Invalid input. Please try again." << endl;
+                cin >> input;
+            }
+            if (oper == "+")
+                current += operand;
+            else if (oper == "-")
+                current -= operand;
+            else if (oper == "*")
+                current *= operand;
+            else if (oper == "/")
+                current /= operand;
+            else
+                current %= operand;
+            activeStack->push(current);
+            while (inactiveStack->size > 0)
+                inactiveStack->pop();
+            inactiveStack->size = 0;
+        }
+        cout << current << endl;
+        cout << "> ";
+        cin >> input;
+        std::transform(input.begin(), input.end(), input.begin(), ::tolower);
+    }
+    cout << "Goodbye" << endl;
 }
--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,131 +1,131 @@
-#include <iostream>
-//Linked list class
-using namespace std;
-
-struct node{
-    char op;
-    int oldValue;
-    int currentValue;
-    node *next;
-};
-
-/*
- * Add new node containing x to the head (front) of the list
- */
-void list::add(char op, int x) {
-    node *temp = new node;
-    temp->op = op;
-    temp->currentValue=x;
-    if (list::head != NULL)
-        temp->oldValue=list::head->currentValue;
-    temp->next=NULL;
-    if(head == NULL) {
-        head = temp;
-        tail = temp;
-        temp = NULL;
-    }
-    else {
-        temp->next=head;
-        head=temp;
-    }
-}
-
-/*
- * Return position of number x in the list
- * First node at position 0, second node at position 1, etc.
- * If x is not found in the list, search should return -1
- */
-int list::search(int x){
-    node *searchNum = nullptr;
-    node *temp = new node;
-    node *current = new node;
-    temp = head;
-    current = head;
-    int i = 0;
-    while(current != NULL && current->currentValue != x){
-        temp = current;
-        current = current->next;
-        i++;
-    }
-    if(current != NULL) {
-        searchNum = current;
-        current = current->next;
-        return i;
-    }
-    else{
-        return -1;
-    }
-}
-
-/*
- * Prints the list out
- */
-void list::print(){
-    node *temp = head;
-    while(temp!=NULL) {
-        cout << temp->currentValue << "\t";
-        temp = temp->next;
-    }
-    cout << endl;
-}
-
-/*
- * Deletes node at specific position
- * If int pos = 0, then the first node in the list is deleted
- * Function does nothing if int pos >= length of list
- */
-void list::remove(int pos){
-    node *current = head;
-    node *previous = new node;
-    for(int i = 0; i < pos; i++) {
-        previous = current;
-        current = current->next;
-    }
-    previous->next = current->next;
-}
-
-/*
- * Prompt user to enter ints, and adds them to linked list (using add function)
- * Print linked list (using print function)
- * Calculate and output sum of elements and average(float)
- *(Must traverse linked list to calculate sum and average)
- * Prompt user for value that should be deleted (using search and remove)
- * Print final linked list
- */
-
-/*
-int main() {
-    list* l = new list();
-    int input;
-    char choice;
-    cout << "Enter a list of integers in order to add them to the linked list. When you are done entering numbers, enter the letter n"<< endl;
-    for (;;) {
-        cin >> choice;
-        if (choice != 'n') {
-            input = atoi(&choice);
-            l->add(input);
-        }
-        else {
-            break;
-        }
-    }
-    l->print();
-
-    int sum = 0;
-    double counter = 0;
-    node* current = l->head;
-    while (current != nullptr) {
-        sum += current->data;
-        counter++;
-        current = current->next;
-    }
-    cout << "The sum of the list is " << sum << endl;
-    cout << "The average of the list is " << (sum / counter) << endl;
-
-    cout << "Enter a value you'd like to delete " << endl;
-    cin >> input;
-    l->remove(l->search(input));
-    l->print();
-    return 0;
-}
- */
+//#include <iostream>
+////Linked list class
+//using namespace std;
+//
+//struct node{
+//    char op;
+//    int oldValue;
+//    int currentValue;
+//    node *next;
+//};
+//
+///*
+// * Add new node containing x to the head (front) of the list
+// */
+//void list::add(char op, int x) {
+//    node *temp = new node;
+//    temp->op = op;
+//    temp->currentValue=x;
+//    if (list::head != NULL)
+//        temp->oldValue=list::head->currentValue;
+//    temp->next=NULL;
+//    if(head == NULL) {
+//        head = temp;
+//        tail = temp;
+//        temp = NULL;
+//    }
+//    else {
+//        temp->next=head;
+//        head=temp;
+//    }
+//}
+//
+///*
+// * Return position of number x in the list
+// * First node at position 0, second node at position 1, etc.
+// * If x is not found in the list, search should return -1
+// */
+//int list::search(int x){
+//    node *searchNum = nullptr;
+//    node *temp = new node;
+//    node *current = new node;
+//    temp = head;
+//    current = head;
+//    int i = 0;
+//    while(current != NULL && current->currentValue != x){
+//        temp = current;
+//        current = current->next;
+//        i++;
+//    }
+//    if(current != NULL) {
+//        searchNum = current;
+//        current = current->next;
+//        return i;
+//    }
+//    else{
+//        return -1;
+//    }
+//}
+//
+///*
+// * Prints the list out
+// */
+//void list::print(){
+//    node *temp = head;
+//    while(temp!=NULL) {
+//        cout << temp->currentValue << "\t";
+//        temp = temp->next;
+//    }
+//    cout << endl;
+//}
+//
+///*
+// * Deletes node at specific position
+// * If int pos = 0, then the first node in the list is deleted
+// * Function does nothing if int pos >= length of list
+// */
+//void list::remove(int pos){
+//    node *current = head;
+//    node *previous = new node;
+//    for(int i = 0; i < pos; i++) {
+//        previous = current;
+//        current = current->next;
+//    }
+//    previous->next = current->next;
+//}
+//
+///*
+// * Prompt user to enter ints, and adds them to linked list (using add function)
+// * Print linked list (using print function)
+// * Calculate and output sum of elements and average(float)
+// *(Must traverse linked list to calculate sum and average)
+// * Prompt user for value that should be deleted (using search and remove)
+// * Print final linked list
+// */
+//
+///*
+//int main() {
+//    list* l = new list();
+//    int input;
+//    char choice;
+//    cout << "Enter a list of integers in order to add them to the linked list. When you are done entering numbers, enter the letter n"<< endl;
+//    for (;;) {
+//        cin >> choice;
+//        if (choice != 'n') {
+//            input = atoi(&choice);
+//            l->add(input);
+//        }
+//        else {
+//            break;
+//        }
+//    }
+//    l->print();
+//
+//    int sum = 0;
+//    double counter = 0;
+//    node* current = l->head;
+//    while (current != nullptr) {
+//        sum += current->data;
+//        counter++;
+//        current = current->next;
+//    }
+//    cout << "The sum of the list is " << sum << endl;
+//    cout << "The average of the list is " << (sum / counter) << endl;
+//
+//    cout << "Enter a value you'd like to delete " << endl;
+//    cin >> input;
+//    l->remove(l->search(input));
+//    l->print();
+//    return 0;
+//}
+// */
